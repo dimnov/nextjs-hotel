f6609a4246f468aa91879217684805bf86da46ec
dimnov <dimnov1512@gmail.com>
2024-07-01 22:59:59 +0300

    set blur, quality to 80 and object cover to the bg image

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import bg from '@/public/bg.png';
 export default function Home() {
   return (
     <main className="mt-24">
-      <Image src={bg} fill alt="Mountains and forests with two cabins" />
+      <Image src={bg} quality={80} fill placeholder="blur" className="object-cover object-top" alt="Mountains and forests with two cabins" />
 
       <div className="relative z-10 text-center">
         <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
