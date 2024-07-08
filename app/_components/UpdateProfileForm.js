ba7adad25703e1bae75690f76e6cd30298187a2f
dimnov <dimnov1512@gmail.com>
2024-07-08 17:16:02 +0300

    Add action to update a guest

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,58 +1,63 @@
 "use client";
 
 import { useState } from "react";
+import { updateGuest } from "../_lib/actions";
+import UpdateProfileFormButton from "./UpdateProfileFormButton";
 
-function UpdateProfileForm({ children }) {
+function UpdateProfileForm({ children, guest }) {
     const [count, setCount] = useState();
 
-    const countryFlag = "pt.jpg";
+    const { fullName, email, nationality, nationalId, countryFlag } = guest;
 
-
-    return (<form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
-        <div className="space-y-2">
-            <label>Full name</label>
-            <input
-                disabled
-                className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
-            />
-        </div>
-
-        <div className="space-y-2">
-            <label>Email address</label>
-            <input
-                disabled
-                className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
-            />
-        </div>
-
-        <div className="space-y-2">
-            <div className="flex items-center justify-between">
-                <label htmlFor="nationality">Where are you from?</label>
-                <img
-                    src={countryFlag}
-                    alt="Country flag"
-                    className="h-5 rounded-sm"
+    return (
+        <form action={updateGuest} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+            <div className="space-y-2">
+                <label>Full name</label>
+                <input
+                    name="fullName"
+                    disabled
+                    defaultValue={fullName}
+                    className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
                 />
             </div>
 
-            {children}
+            <div className="space-y-2">
+                <label>Email address</label>
+                <input
+                    name="email"
+                    disabled
+                    defaultValue={email}
+                    className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+                />
+            </div>
 
-        </div>
+            <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                    <label htmlFor="nationality">Where are you from?</label>
+                    <img
+                        src={countryFlag}
+                        alt="Country flag"
+                        className="h-5 rounded-sm"
+                    />
+                </div>
 
-        <div className="space-y-2">
-            <label htmlFor="nationalID">National ID number</label>
-            <input
-                name="nationalID"
-                className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-            />
-        </div>
+                {children}
 
-        <div className="flex justify-end items-center gap-6">
-            <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-                Update profile
-            </button>
-        </div>
-    </form>);
+            </div>
+
+            <div className="space-y-2">
+                <label htmlFor="nationalID">National ID number</label>
+                <input
+                    name="nationalID"
+                    defaultValue={nationalId}
+                    className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+                />
+            </div>
+
+            <div className="flex justify-end items-center gap-6">
+                <UpdateProfileFormButton />
+            </div>
+        </form>);
 }
 
 export default UpdateProfileForm;
