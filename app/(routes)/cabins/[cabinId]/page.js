862e7c5d9a68fe4786ea9b3c2061faff1367b5b9
dimnov <dimnov1512@gmail.com>
2024-11-17 21:11:42 +0200

    Update page.js

--- app/(routes)/cabins/[cabinId]/page.js
+++ app/(routes)/cabins/[cabinId]/page.js
@@ -5,34 +5,33 @@ import { getCabin, getCabinsId } from "@/app/_lib/data-service";
 import { Suspense } from "react";
 
 export async function generateMetadata({ params }) {
-    const { name } = await getCabin(params.cabinId);
+  const { name } = await getCabin(params.cabinId);
 
-    return { title: `Cabin ${name}` }
+  return { title: `Cabin ${name}` };
 }
 
 export async function generateStaticParams() {
-    const ids = await getCabinsId();
+  const ids = await getCabinsId();
 
-    return ids;
+  return ids;
 }
 
 export default async function Page({ params }) {
-    const cabin = await getCabin(params.cabinId);
+  const cabin = await getCabin(params.cabinId);
 
-    return (
-        <div className="max-w-6xl mx-auto mt-8">
-            <Cabin cabin={cabin} />
+  return (
+    <div className="max-w-6xl mx-auto mt-8">
+      <Cabin cabin={cabin} />
 
-            <div>
-                <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
-                    Reserve {cabin.name} today. Pay on arrival.
-                </h2>
+      <div>
+        <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
+          Reserve {cabin.name} today. Pay on arrival.
+        </h2>
 
-                <Suspense fallback={<Spinner />}>
-                    <Reservation cabin={cabin} />
-                </Suspense>
-
-            </div>
-        </div>
-    );
+        <Suspense fallback={<Spinner />}>
+          <Reservation cabin={cabin} />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
